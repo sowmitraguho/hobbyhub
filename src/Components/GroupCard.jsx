993137554c54683adfde058b666f41c13e4c098f
Sowmitra Guha <sowmmochy@gmail.com>
2025-05-23 15:26:59 +0600

    featured group section done in home

---
+++ src/Components/GroupCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const GroupCard = ({ group }) => {
+    //console.log(group);
+    const { _id, name, hobbyCategory, description, photoURL, date } = group;
+    return (
+        <div >
+            <div className="card bg-base-100 w-96 h-96 shadow-sm">
+                <figure>
+                    <img className='w-96'
+                        src={photoURL}
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">{name}
+                        <div className="badge badge-secondary">{hobbyCategory}</div>
+                    </h2>
+                    <p>{description}</p>
+                    <p>Last Date to Join: {date}</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">View Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default GroupCard;
--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        loader: () => fetch('http://localhost:3000/groups'),
         Component: Home
       },
       {
